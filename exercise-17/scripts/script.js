432186cc95002a1749c14badfa99ed73c5b19d5f
Thanga Akilan <thanga.akilan@cdw.com>
2025-04-24 18:16:37 +0530

    fix: font

--- exercise-17/scripts/script.js
+++ exercise-17/scripts/script.js
@@ -4,7 +4,7 @@ import {locations} from "../public/locations.js";
 // Tab from Jquery UI
 $( "#tabs" ).tabs();
 
-
+// Accordion from Jquery UI
 $( function() {
     $( "#accordion" ).accordion({
       collapsible: true,
@@ -12,28 +12,44 @@ $( function() {
     });
   } );
 
-$("h3").on("click",function(){
+
+// Accordion Title
+$(".accordion-title").on("click",function(){
 $(this).children('i').toggleClass('fa-caret-down');
 })
 
-
+// Base Container
 let baseContainer = document.createElement("div");
 baseContainer.classList.add("tab-3-base-container");
 
-function createCard(location){
+// Create Card for each location
+locations.forEach(location=>{
+    createCard(location);
+})
 
+
+// Append to root container
+let root = document.getElementById("tabs-3")
+root.appendChild(baseContainer)
+
+
+// Create Card
+function createCard(location){
+    // Card div
     let card = document.createElement("div")
     card.classList.add("card")
 
-
+    // Image for Flag
     let flagImage = document.createElement("img")
     flagImage.classList.add("card-image")
 
+    // Wrapper for image flag
     let flagWrapper = document.createElement("div")
     flagWrapper.classList.add("card-wrapper")
     flagWrapper.appendChild(flagImage)
 
 
+    // Set the image src for the card
     switch(location.country){
         case "United States":
             flagImage.setAttribute("src","../public/usa.jpg")
@@ -49,30 +65,27 @@ function createCard(location){
 
     }
     
+    // State text
     let stateElement = document.createElement("span")
     stateElement.textContent = location.state;
     stateElement.classList.add("card-state")
 
+    // City text
     let cityElement = document.createElement("span")
     cityElement.textContent = location.city;
     cityElement.classList.add("card-city")
 
-
+    // Contact text
     let contactElement = document.createElement("span")
     contactElement.textContent = location.contact;
 
+    // Append all cotents to card div
     card.appendChild(flagWrapper)
     card.appendChild(stateElement)
     card.appendChild(cityElement)
     card.appendChild(contactElement)
 
+    // Append card to base container
     baseContainer.appendChild(card)
 }
 
-
-locations.forEach(location=>{
-    createCard(location);
-})
-
-let root = document.getElementById("tabs-3")
-root.appendChild(baseContainer)
