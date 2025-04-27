3f1434bd179ab009dda7583d6893f14f1b14d1fd
Thanga Akilan <thanga.akilan@cdw.com>
2025-04-27 18:30:11 +0530

    feat: mobile and web

--- exercise-18/scripts/script.js
+++ exercise-18/scripts/script.js
@@ -1,4 +1,39 @@
 
-$.getJson("../public/video.json", function(){
-    conaole.log("hello");
-})
+$(document).ready(function() {
+    $.getJSON('../public/video.json', function(data) {
+        console.log(data);
+        $('#videoComponent').attr("src",data.videoUrl)
+        $('#videoTitle').text(data.title)
+        $('#videoDescription').text(data.description);
+      $.each(data.comments, function(index, comment) {
+        console.log(index, comment)
+        const commentUI = `
+        <div class="comment-component">
+                <div class="comment-img-wrapper">
+                    <img src=${'../public/'+comment.image}>
+                </div>
+                <div class="comment-content">
+                    <p id="commentor-name" class="commentor-name">${comment.name}</p>
+                    <p class="comment-description">${comment.comment}</p>
+                </div>
+            </div>
+        `;
+
+        $('#commentsContainer').append(commentUI);
+      });
+    });
+
+    $.getJSON('../public/posters.json',function(posters){
+        $.each(posters, function(index,poster){
+            console.log(poster)
+            const posterUI = `
+            <div class="poster-wrapper">
+                <img src=${poster.imageUrl} alt=${poster.title}/>
+            </div>`
+            $("#posterSection").append(posterUI);
+        })
+    })
+  });
+
+
+
